dd7257b7069eb6d6189edb6b052b3d060b1af70d
TungDV <74850310+DaoVanTung@users.noreply.github.com>
2023-09-15 14:47:50 +0700

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'MKDC'
+copyright = '2023, Tecotec'
+author = 'Tecotec Group'
 
 release = '0.1'
 version = '0.1.0'
